deae20f8020c18bb2fc202840b9eb67fdeceeda8
killi1812 <cvok.fran@gmail.com>
2024-12-08 13:41:18 +0100

    Day 8 almost

--- day8/main.go
+++ day8/main.go
@@ -6,54 +6,12 @@ import (
 	"strings"
 )
 
+const empty = '.'
+const antinode = '#'
+
 type mapa struct {
 	m             [][]rune
 	height, width int
-}
-
-type cords struct {
-	X int
-	Y int
-}
-
-type antennasPair struct {
-	first  cords
-	second cords
-}
-
-func newMapa(arr [][]rune) *mapa {
-	return &mapa{
-		m:      arr,
-		height: len(arr),
-		width:  len(arr[0]),
-	}
-}
-
-func main() {
-	input := read()
-	m := newMapa(stringsToRunes(input))
-	m.printMap()
-	pairs := m.findAntenaPairs()
-	for _, pair := range pairs {
-		pair.print(*m)
-	}
-}
-
-func (this *antennasPair) print(m mapa) {
-	char, err := m.at(this.first)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Printf("Pair: %s : %v, %v\n", string(char), this.first, this.second)
-}
-
-func stringsToRunes(strArray []string) [][]rune {
-	var runeArray [][]rune
-	for _, str := range strArray {
-		runeArray = append(runeArray, []rune(str))
-	}
-
-	return runeArray
 }
 
 func (this *mapa) printMap() {
@@ -65,15 +23,41 @@ func (this *mapa) printMap() {
 	}
 }
 
+func (this *mapa) count(a rune) int {
+	count := 0
+	for _, row := range this.m {
+		for _, char := range row {
+			if char == a {
+				count++
+			}
+		}
+	}
+	return count
+}
+
 func (this *mapa) at(loc cords) (rune, error) {
-	if this.height <= loc.Y || this.width <= loc.X {
+	if this.height <= loc.Y || this.width <= loc.X || loc.X < 0 || loc.Y < 0 {
 		return '.', fmt.Errorf("Out of bounds")
 	}
 	return this.m[loc.Y][loc.X], nil
 
 }
 
-const empty = '.'
+func (this *mapa) change(char rune, loc cords) error {
+	if this.height <= loc.Y || this.width <= loc.X || loc.X < 0 || loc.Y < 0 {
+		return fmt.Errorf("Out of bounds")
+	}
+	this.m[loc.Y][loc.X] = char
+	return nil
+}
+
+func newMapa(arr [][]rune) *mapa {
+	return &mapa{
+		m:      arr,
+		height: len(arr),
+		width:  len(arr[0]),
+	}
+}
 
 func (this *mapa) findAntenaPairs() []antennasPair {
 	ret := []antennasPair{}
@@ -102,6 +86,92 @@ func (this *mapa) findPairsForAntena(find rune, loc cords) []antennasPair {
 		}
 	}
 	return ret
+}
+
+type cords struct {
+	X int
+	Y int
+}
+
+func (this cords) add(loc cords) cords {
+	return cords{
+		X: this.X + loc.X,
+		Y: this.Y + loc.Y,
+	}
+}
+
+func (this cords) subtract(loc cords) cords {
+	return cords{
+		X: this.X - loc.X,
+		Y: this.Y - loc.Y,
+	}
+}
+
+type antennasPair struct {
+	first  cords
+	second cords
+}
+
+func (this *antennasPair) print(m mapa) {
+	char, err := m.at(this.first)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("Pair: %s : %v, %v\n", string(char), this.first, this.second)
+}
+
+func stringsToRunes(strArray []string) [][]rune {
+	var runeArray [][]rune
+	for _, str := range strArray {
+		if str == "" {
+			continue
+		}
+		runeArray = append(runeArray, []rune(str))
+	}
+
+	return runeArray
+}
+
+func diffParis(pair antennasPair) cords {
+	return cords{
+		X: pair.second.X - pair.first.X,
+		Y: pair.second.Y - pair.first.Y,
+	}
+}
+
+func main() {
+	input := read()
+	m := newMapa(stringsToRunes(input))
+	m.printMap()
+	pairs := m.findAntenaPairs()
+	for _, pair := range pairs {
+		pair.print(*m)
+	}
+	zad01(pairs, m)
+	m.printMap()
+	fmt.Printf("overlapCount: %v\n", overlapCount)
+	fmt.Printf("Count of antinodes is: %d\n", m.count(antinode)+overlapCount)
+}
+
+var overlapCount = 0
+
+func zad01(arr []antennasPair, m *mapa) {
+	for _, pair := range arr {
+		diff := diffParis(pair)
+		a, b := pair.first.subtract(diff), pair.second.add(diff)
+		if tmp, _ := m.at(a); tmp == empty || tmp == antinode {
+			if tmp == antinode {
+				overlapCount++
+			}
+			m.change(antinode, a)
+		}
+		if tmp, _ := m.at(b); tmp == empty || tmp == antinode {
+			if tmp == antinode {
+				overlapCount++
+			}
+			m.change(antinode, b)
+		}
+	}
 }
 
 func read() []string {
